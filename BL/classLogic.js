12e22ce1f9377bcc4e301280149bcede8022920f
Damri <damri@hp.com>
2022-06-29 19:28:31 +0300

    add get classes BL

--- BL/classLogic.js
+++ BL/classLogic.js
@@ -1,5 +1,7 @@
 const classController = require("DL/controllers/classController.js");
 
-exports.getClass=()=>{
-    return classController.read({})
+exports.getAllClassForTeacher = async (teacherID) => {
+    const classes = await classController.read({ teacherId: teacherID })
+    if (classes.length === 0) throw ({ code: 400, msg: "there are no classes" })
+    return classes
 }
